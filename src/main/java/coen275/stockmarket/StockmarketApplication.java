60c5dde02a204c1508c294a34bf91eefa876cdd1
ShuangchangLi <89339613+ShuangchangLi@users.noreply.github.com>
2022-05-24 19:27:29 -0700

    security changed

--- src/main/java/coen275/stockmarket/StockmarketApplication.java
+++ src/main/java/coen275/stockmarket/StockmarketApplication.java
@@ -2,9 +2,10 @@ package coen275.stockmarket;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
+@SpringBootApplication( exclude = { SecurityAutoConfiguration.class } )
 public class StockmarketApplication {
 
     public static void main(String[] args) {
